8dbbfde1e01e7f4c9b84dc54b336d5c0a5c9f065
aleksapetrovic2907 <66926460+aleksapetrovic2907@users.noreply.github.com>
2024-12-16 00:36:05 +0100

    Update in Infrastructure layer: Customize Swagger API metadata

--- Infrastructure/Program.cs
+++ Infrastructure/Program.cs
@@ -2,6 +2,7 @@ using Application.Services;
 using Application.Validators;
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using Microsoft.OpenApi.Models;
 using Serilog;
 
 Log.Logger = new LoggerConfiguration()
@@ -25,7 +26,20 @@ builder.Services.AddFluentValidationClientsideAdapters();
 // Add WebAPI controllers
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Shipment Tracking API",
+        Version = "v1",
+        Description = "An API for managing shipments in a logistics system.",
+        Contact = new OpenApiContact
+        {
+            Name = "Aleksa Petrovic",
+            Email = "aleksa.petrovic2907@gmail.com"
+        }
+    });
+});
 
 var app = builder.Build();
 

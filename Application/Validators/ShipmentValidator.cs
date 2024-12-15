dadb9ba31cb2002db8fd5ea01ec42d4349e40334
aleksapetrovic2907 <66926460+aleksapetrovic2907@users.noreply.github.com>
2024-12-15 23:23:48 +0100

    Add in Application layer: `Shipment` model validation using FluentValidation

---
+++ Application/Validators/ShipmentValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using Domain.Models;
+
+namespace Application.Validators
+{
+    public class ShipmentValidator : AbstractValidator<Shipment>
+    {
+        public ShipmentValidator()
+        {
+            RuleFor(s => s.Name)
+                .NotEmpty().WithMessage("Shipment name cannot be empty");
+
+            RuleFor(s => s.DeliveredAt)
+                .GreaterThanOrEqualTo(DateTime.UtcNow)
+                .When(s => s.DeliveredAt.HasValue)
+                .WithMessage("Delivery date must be in the future");
+
+            RuleFor(s => s.Status)
+                .IsInEnum().WithMessage("Invalid shipment status");
+        }
+    }
+}

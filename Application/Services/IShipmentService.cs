11a2fbfe8ad1189d4d0c473261b1b25135bc2fac
aleksapetrovic2907 <66926460+aleksapetrovic2907@users.noreply.github.com>
2024-12-15 21:38:45 +0100

    Add Application layer: `IShipmentService` and `ShipmentService`

---
+++ Application/Services/IShipmentService.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Models;
+
+namespace Application.Services
+{
+    public interface IShipmentService
+    {
+        IEnumerable<Shipment> GetAllShipments();
+        Shipment GetShipmentById(Guid id);
+        void AddShipment(Shipment shipment);
+        void UpdateShipment(Shipment shipment);
+        void DeleteShipment(Guid id);
+    }
+}
---
+++ Application/Services/ShipmentService.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Models;
+
+namespace Application.Services
+{
+    public class ShipmentService : IShipmentService
+    {
+        private readonly List<Shipment> _shipments = new();
+
+        public IEnumerable<Shipment> GetAllShipments() => _shipments;
+
+        public Shipment GetShipmentById(Guid id) => _shipments.FirstOrDefault(s => s.Id == id);
+
+        public void AddShipment(Shipment shipment)
+        {
+            shipment.Id = Guid.NewGuid();
+            shipment.DatumKreiranja = DateTime.UtcNow;
+            _shipments.Add(shipment);
+        }
+
+        public void UpdateShipment(Shipment shipment)
+        {
+            var targetShipment = GetShipmentById(shipment.Id);
+
+            if (targetShipment != null)
+            {
+                targetShipment.Naziv = shipment.Naziv;
+                targetShipment.Status = shipment.Status;
+                targetShipment.DatumIsporuke = shipment.DatumIsporuke;
+            }
+        }
+
+        public void DeleteShipment(Guid id) => _shipments.RemoveAll(s => s.Id == id);
+    }
+}

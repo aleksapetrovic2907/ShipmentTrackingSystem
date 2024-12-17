be0da0b95ea65899b40c3d8f55c8846ef0e62edc
aleksapetrovic2907 <66926460+aleksapetrovic2907@users.noreply.github.com>
2024-12-17 16:31:48 +0100

    Fix in Application layer: Validate creation and delivery dates in `ShipmentValidator` using the Date only

--- Application/Validators/ShipmentValidator.cs
+++ Application/Validators/ShipmentValidator.cs
@@ -11,12 +11,12 @@ namespace Application.Validators
                 .NotEmpty().WithMessage("Shipment name cannot be empty");
 
             RuleFor(s => s.DeliveredAt)
-                .GreaterThanOrEqualTo(s => s.CreatedAt)
+                .GreaterThanOrEqualTo(s => s.CreatedAt.Date)
                 .When(s => s.DeliveredAt.HasValue)
                 .WithMessage("Delivery date must be on or after the creation date");
 
             RuleFor(s => s.DeliveredAt)
-                .LessThanOrEqualTo(s => DateTime.UtcNow)
+                .LessThanOrEqualTo(s => DateTime.UtcNow.Date)
                 .When(s => s.DeliveredAt.HasValue)
                 .WithMessage("Delivery date cannot be in the future");
 
